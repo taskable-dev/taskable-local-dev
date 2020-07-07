a70e3d31d96aa78734e6c0688e858a9919971e6b
Darryl Norris <admin@darrylnorris.com>
2020-07-07 09:39:12 -0700

    Adding some parameters for stealth

--- src/index.ts
+++ src/index.ts
@@ -226,7 +226,18 @@ export const task = {
     console.log('running')
 
     puppeteer.use(StealthPlugin())
-    let browser: any = await puppeteer.launch()
+    let browser: any = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+      ],
+    })
 
     // start page
     let page = await browser.newPage()
